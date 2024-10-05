91096875c456b081b27347c04a1959ebef2100b7
MariaNagornaya1980 <mariawright205@gmail.com>
2024-10-05 16:17:46 -0700

    added links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/MariaNagornaya1980"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Maria Nagornaya
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/MariaNagornaya1980/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://react-weather-app-masha.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
