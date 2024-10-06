f67d34d51f8ed00849efbd3746d6406b8c37e5a0
MariaNagornaya1980 <mariawright205@gmail.com>
2024-10-06 13:32:24 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,23 +5,29 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/MariaNagornaya1980" target="_blank">
-          Maria Nagornaya
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/MariaNagornaya1980/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a href="https://react-weather-app-masha.netlify.app/" target="_blank">
-          hosted on Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/MariaNagornaya1980" target="_blank">
+            Maria Nagornaya
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/MariaNagornaya1980/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://react-weather-app-masha.netlify.app/"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
